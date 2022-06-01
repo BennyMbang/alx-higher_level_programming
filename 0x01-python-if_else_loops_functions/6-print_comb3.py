17eb2a50b4ab7f1b97d6b47af3b6202336dea9be
BennyMbang <mbenny511@gmail.com>
2022-06-01 15:13:38 -0700

    task edited

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range (10):
+for i in range(10):
     for n in range(i, 10):
         if n != i and (i != 8 or n != 9):
             print("{}{}, ".format(i, n), end="")
