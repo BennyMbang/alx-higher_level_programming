594eb06b3d85ceee88eff6cc8bcd12381019e76a
BennyMbang <mbenny511@gmail.com>
2022-06-06 10:24:40 -0700

    task edited

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    my_string = translate({ord('c'): None})
 
-    return (my_string)
+def no_c(my_string):
+    for c in list(my_string):
+        return "".translate({ord('c' 'C'): None})
