e09922a3dbf42e0b755cdbf1defd92858d30c458
BennyMbang <mbenny511@gmail.com>
2022-06-08 15:29:23 -0700

    tasks added

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
-    return (a_dictionary)
+def print_sorted_dictionary(a_dictionary):
+    for llave in sorted(a_dictionary):
+        print("{}: {}".format(llave, a_dictionary[llave]))
