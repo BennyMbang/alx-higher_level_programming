60d4a330e5c6710029babbe314ff8a9849171fb0
BennyMbang <mbenny511@gmail.com>
2022-09-06 05:23:02 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const bennyMbang = 'Python is cool';
+const myLang = 'JavaScript is amazing';
+console.log(myVar);
+console.log(bennyMbang);
+console.log(myLang);
