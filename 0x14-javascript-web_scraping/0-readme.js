2646f59ac5b0a8294d80ab0cc2303f4026cfe042
BennyMbang <mbenny511@gmail.com>
2022-10-18 05:59:22 -0700

    files updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 /* reads and prints the content of a file */
 const fs = require('fs');
 
-fs.readFile(process.argv[2],'utf8', function(err, data) {
-    if (err) {
-	console.log(err);
-    }
-    console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
 });
