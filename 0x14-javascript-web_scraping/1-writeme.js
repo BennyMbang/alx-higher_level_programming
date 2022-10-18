3b48401de23d691e1540361680c3f6c2fb42b4dc
BennyMbang <mbenny511@gmail.com>
2022-10-18 05:57:41 -0700

    file updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 const args = process.argv;
 fs.writeFile(args[2], args[3], function (err) {
-    if (err) {
-	console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 });
