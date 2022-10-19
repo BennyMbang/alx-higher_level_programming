e351f9076a124e293da1a88ec9ad8f0a8c7a5473
BennyMbang <mbenny511@gmail.com>
2022-10-18 18:14:39 -0700

    file added

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/:id' + id
 
-request(url, function (error, response, body) {
-    if (error) {
-	console.log(error);
-    }
-    const data = JSON.parse(body);
-    console.log(data.title);
-});
+if (process.argv.length > 2) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  request(url, (err, res, body) => {
+    if (err) console.log(err);
+
+    console.log(JSON.parse(body).title);
+  });
+}
